f2b1a649d7a264fc13a03c2d79a2a3ab7811f1ca
robinyang_beijing@163.com <robinyang_beijing@163.com>
2018-03-07 15:32:29 +0800

    实现登录功能

--- manager/src/main/resources/web/dist/js/loginin.js
+++ manager/src/main/resources/web/dist/js/loginin.js
@@ -1,10 +1,10 @@
-
+var  reseturl="http://127.0.0.1:8080/index.html";
 function loginMQ(){
     var adm_name=document.getElementById("lname").value;
     var adm_pwd=document.getElementById("lpwd").value;
     if (adm_name==='lkx'&&adm_pwd ==='1'){
-        alert("登录成功");
-        window.location.href='http://127.0.0.1:8080/index.html';
+         // window.location.href=reseturl;
+         window.open(reseturl);
     }else{
         alert("登录失败！请检查您输入的账号和密码是否正确");
     }
